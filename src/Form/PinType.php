92ba256c12eb35842b00407d84072c2bdcc550de
Mariam <mariamomri.dev@gmail.com>
2026-09-10 12:00:51 +0200

    Add Pin templates (index, show, create, edit) with dark themed card layout

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Pin;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,18 +16,18 @@ class PinType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('imageName')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('title', TextType::class, [
+                'label' => 'Title'
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false
             ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('imageName', UrlType::class, [
+                'label' => 'Image URL'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save'
             ])
         ;
     }
