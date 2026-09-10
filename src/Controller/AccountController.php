be7706c77b0f3be0c6ea49791dc9f4302dce5cdb
Mariam <mariamomri.dev@gmail.com>
2026-09-10 11:42:36 +0200

    Add AccountController with show and edit actions plus basic templates

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AccountController extends AbstractController
+{
+    #[Route('/account', name: 'app_account')]
+    public function show(): Response
+    {
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'You must login to see your account !');
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('account/show.html.twig');
+    }
+
+    #[Route('/account/edit', name: 'app_account_edit')]
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'You must login to edit your account !');
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Account successfully updated !');
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/edit.html.twig', [
+            'user' => $user,
+            'userForm' => $form->createView()
+        ]);
+    }
+}
